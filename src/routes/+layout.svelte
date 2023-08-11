725ce772d10b917e96d61cb60e80a7499a2c2b21
siobe34 <siobe14@hotmail.com>
2023-08-11 13:24:02 -0400

    grid layout for navbar and main content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,11 @@
+<nav
+	class="relative flex gap-4 items-center justify-end py-4 border-b-4 border-blue-400 bg-slate-200 md:justify-center"
+>
+</nav>
+<main class="">
 	<slot />
+</main>
+
 <style>
 	@tailwind base;
 	@tailwind components;
