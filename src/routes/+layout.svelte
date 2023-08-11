af9309a4f0dc9252fd083009533831584faa9c20
siobe34 <siobe14@hotmail.com>
2023-08-11 13:25:55 -0400

    FTR: hide/show nav menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	let showMenu = false;
 </script>
+
 <nav
 	class="relative flex gap-4 items-center justify-end py-4 border-b-4 border-blue-400 bg-slate-200 md:justify-center"
 >
+	{#if showMenu}
 		<ul
 			class="absolute top-full flex flex-col gap-4 items-center justify-center pb-4 w-full z-50 border-b-4 border-blue-500 bg-inherit md:static md:flex-row md:p-0 md:border-none"
 		>
@@ -20,6 +22,7 @@
 				<a href="#top">Resume</a>
 			</li>
 		</ul>
+	{/if}
 	<button
 		class="flex items-center px-4 py-2 justify-center md:hidden"
 		on:click={() => (showMenu = !showMenu)}>Menu</button
