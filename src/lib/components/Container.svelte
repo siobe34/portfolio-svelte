6527d81075d8bab4e7912636e068d965469dbcdf
siobe34 <siobe14@hotmail.com>
2023-08-13 17:12:16 -0400

    FTR: responsive page content container

---
+++ src/lib/components/Container.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	let props = { ...$$restProps };
+	export let classOverrides = '';
+</script>
+
+<article
+	class={`${classOverrides} flex flex-col gap-4 justify-center items-center w-11/12 md:w-4/5 lg:w-3/5 my-4`}
+	{...props}
+>
+	<slot />
+</article>
