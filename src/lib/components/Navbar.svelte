a33b025c5524904094397439b285565ddc574e63
siobe34 <siobe14@hotmail.com>
2023-08-13 17:11:01 -0400

    FTR: theme toggling

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -4,6 +4,17 @@
 
 	// * Whether to show the mobile menu or hide it
 	let showMenu = false;
+
+	// * Dark mode status
+	let darkMode = false;
+
+	// * Func to add/remove '.dark' class
+	const toggleTheme = () => {
+		darkMode = !darkMode;
+
+		const appDiv = document.getElementsByClassName('app')[0];
+		appDiv?.classList.toggle('dark');
+	};
 </script>
 
 <nav
@@ -32,6 +43,20 @@
 			{/each}
 		</ul>
 	{/key}
+	<span class="flex items-center justify-center gap-2 h-full mr-4">
+		<i class="far fa-moon" />
+		<button
+			class="relative flex flex-col justify-center w-14 h-full rounded-full border border-border"
+			on:click={toggleTheme}
+		>
+			<span
+				class="absolute w-[20px] h-[20px] mx-1 rounded-full bg-primary transition-all"
+				class:left-[28px]={darkMode}
+				class:left-[0px]={!darkMode}
+			/>
+		</button>
+		<i class="far fa-sun" />
+	</span>
 	<button
 		class="flex items-center px-4 py-2 justify-center md:hidden"
 		on:click={() => (showMenu = !showMenu)}
