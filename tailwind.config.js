29d90cfdc77005c8035374c4195033eb0c8547e1
siobe34 <siobe14@hotmail.com>
2023-08-13 17:06:51 -0400

    FTR: custom tailwind colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			colors: {
+				bg: 'rgb(var(--bg) / <alpha-value>)',
+				border: 'rgb(var(--border) / <alpha-value>)',
+				onBg: 'rgb(var(--onBg) / <alpha-value>)',
+				primary: 'rgb(var(--primary) / <alpha-value>)',
+				onPrim: 'rgb(var(--onPrim) / <alpha-value>)',
+				secondary: 'rgb(var(--secondary) / <alpha-value>)',
+				onSec: 'rgb(var(--onSec) / <alpha-value>)'
+			}
+		}
 	},
 	plugins: []
 };
