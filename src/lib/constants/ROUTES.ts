a9fa54a2aa588f6e5f01655544c227f4aecc3d28
ebad <siobe14@hotmail.com>
2026-07-07 00:29:28 -0400

    update link to resume

--- src/lib/constants/ROUTES.ts
+++ src/lib/constants/ROUTES.ts
@@ -2,5 +2,5 @@ export const ROUTES = [
 	{ url: '#about-me', name: 'About Me' },
 	{ url: '#projects', name: 'Projects' },
 	{ url: '#technical-skills', name: 'Technical Skills' },
-	{ url: 'https://v1ibadrashid.vercel.app/Resume_Ibad-Rashid.pdf', name: 'Resume' }
+	{ url: '/Resume_IbadRashid_2026-07-02.pdf', name: 'Resume' }
 ];
